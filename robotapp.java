e7c0bc4cd40c0223c9895350c4205aa23716528d
Jude <JudePWhite@gmail.com>
2024-05-23 14:59:42 -0400

    h

--- robotapp.java
+++ robotapp.java
@@ -3,7 +3,19 @@ import java.awt.*;
 public class robotapp{
     public static void main(String[] args) throws Exception{
         Robot jud = new Robot();
-        jud.mouseMove(1870,1220);
+        jud.mouseMove(1880,515);
         jud.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        jud.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        jud.mouseMove(1880,990);
+        jud.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        jud.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(1000);
+        jud.mouseMove(1025,50);
+        jud.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        jud.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        jud.mouseMove(1020,165);
+        jud.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        jud.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
     }
 }
