c431601002449f21f87e2a3fff4d34386ba5133c
Jude <JudePWhite@gmail.com>
2024-05-28 14:35:31 -0400

    Update robotapp.java

--- robotapp.java
+++ robotapp.java
@@ -1,5 +1,9 @@
 import java.awt.event.InputEvent;
 import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class robotapp{
     public static void main(String[] args) throws Exception{
         Robot jud = new Robot();
@@ -13,9 +17,20 @@ public class robotapp{
         jud.mouseMove(1025,50);
         jud.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         jud.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(1000);
         jud.mouseMove(1020,165);
         jud.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         jud.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Rectangle bounds = new Rectangle(screenSize);
+        BufferedImage image= jud.createScreenCapture(bounds); 
+        File file = new File("myScreenShot.png");
+        ImageIO.write(image,"png", file);
+        System.out.println("The screenshot is in " + file.getPath()); 
 
+        jud.mouseMove(1280,80);
+        Thread.sleep(2000);
+        jud.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        jud.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
     }
 }
